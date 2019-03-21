02ba8eab95594838fa2b6749efcbd0c7bf77a4c0
nihilism <nihilism.core@gmail.com>
2019-03-21 20:04:50 +0700

    learn component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Todo_list from "./components/Todo_list";
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <Todo_list />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
---
+++ src/components/Todo_list.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Todo_list extends Component {
+  render() {
+    return (
+      <div className="todo_list">
+        <ul>
+          <li>Di cho</li>
+          <li>Nau com</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Todo_list;
